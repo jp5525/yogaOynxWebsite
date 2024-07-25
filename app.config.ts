1b3c9ad2d5dc02dc6d1d0d5b10e12d5b73d348ec
Jared Piro <jp5525@yahoo.com>
2024-07-24 23:37:44 -0400

    Added favicon and CNAME

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
       ssr: true,
       server: {
         baseURL: process.env.BASE_PATH,
-        preset: "static"
-      }
+        preset: "static",
+      },
+       
   });
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,7 +8,7 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.png" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />       
